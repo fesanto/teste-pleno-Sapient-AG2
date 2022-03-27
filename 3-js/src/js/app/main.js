c75764848980f345fccf9bdbdbe206776055d0c5
Fernanda Santo <fgesanto@gmail.com>
2022-03-27 19:16:29 -0300

    funcionalidade do carrossel feita no main.js

--- 3-js/src/js/app/main.js
+++ 3-js/src/js/app/main.js
@@ -10,3 +10,27 @@ playVideo();
 openAccordion();
 openWiki();
 openModal();
+
+// Funcionalidade do carrossel
+let sliderImages = document.querySelectorAll("#carousel .item"),
+    current = 0;
+
+function reset() {
+    for (let i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.display = "none";
+    }
+}
+
+function slide() {
+    reset();
+    sliderImages[current + 1].style.display = "block";
+    current++;
+}
+
+function carousel() {
+    if (current === sliderImages.length - 1) {
+        current = -1;
+    }
+    slide();
+};
+setInterval(carousel, 1800);
